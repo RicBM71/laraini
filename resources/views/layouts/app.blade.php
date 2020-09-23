3514017eef5fe5d3aa3ee47430720d798786286a
RicBM71 <rbartolome@sanaval.com>
2020-09-23 10:09:27 +0200

    app.blade ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
@@ -19,7 +15,8 @@
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
 </head>
 <body>
     <div id="app">
@@ -80,5 +77,6 @@
             </main>
         </v-app>
     </div>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
